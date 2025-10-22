9fc27fc75fba32adac112a2b707a360b3cc941bf
yzyl1114 <401716829@qq.com>
2025-10-22 12:36:13 +0800

    更新考试内容和时间

--- docs/js/exams.js
+++ docs/js/exams.js
@@ -3,43 +3,43 @@ const exams = [
     {
         id: 1,
         name: "中小学教师资格考试（笔试）",
-        date: "2025-03-08T09:00:00+08:00",
+        date: "2025-09-13T09:00:00+08:00",
         image: "images/teacher_exam.jpg"
     },
     {
         id: 2,
         name: "全国计算机等级考试",
-        date: "2025-03-29T09:00:00+08:00",
+        date: "2025-09-20T09:00:00+08:00",
         image: "images/computer_exam.jpg"
     },
     {
         id: 3,
         name: "中小学教师资格考试（面试）",
-        date: "2025-05-17T09:00:00+08:00",
+        date: "2025-12-06T09:00:00+08:00",
         image: "images/teacher_interview.jpg"
     },
     {
         id: 4,
         name: "同等学力全国统考",
-        date: "2025-05-18T09:00:00+08:00",
+        date: "2025-05-24T09:00:00+08:00",
         image: "images/samelevel_exam.jpg"
     },
     {
         id: 5,
-        name: "英语四六级考试（口语）",
-        date: "2025-05-24T09:00:00+08:00",
+        name: "英语四级考试（口语）",
+        date: "2025-11-22T09:00:00+08:00",
         image: "images/cet_spoken.jpg"
     },
     {
         id: 6,
         name: "高考",
-        date: "2025-06-07T09:00:00+08:00",
+        date: "2026-06-07T09:00:00+08:00",
         image: "images/gaokao.jpg"
     },
     {
         id: 7,
-        name: "英语四六级考试（笔试）",
-        date: "2025-06-14T09:00:00+08:00",
+        name: "英语四级考试（笔试）",
+        date: "2025-12-13T09:00:00+08:00",
         image: "images/cet_written.jpg"
     },
     {
@@ -51,13 +51,13 @@ const exams = [
     {
         id: 9,
         name: "法律职业资格考试（客观题）",
-        date: "2025-09-13T09:00:00+08:00",
+        date: "2026-09-12T09:00:00+08:00",
         image: "images/law_objective.jpg"
     },
     {
         id: 10,
         name: "法律职业资格考试（主观题）",
-        date: "2025-10-12T09:00:00+08:00",
+        date: "2026-10-11T09:00:00+08:00",
         image: "images/law_subjective.jpg"
     },
     {
@@ -69,7 +69,7 @@ const exams = [
     {
         id: 12,
         name: "硕士研究生招生考试（初试）",
-        date: "2025-12-21T09:00:00+08:00",
+        date: "2025-12-20T09:00:00+08:00",
         image: "images/graduate_exam.jpg"
     }
 ];
